e1c6c73ce4c1a0bd4b3933abb176f83c8d3d22a2
Loïs <abeetlott@gmail.com>
2026-03-05 17:11:09 +0100

    First commit of the project. Building via Visual Studio

---
+++ ZulaOnUE/Source/ZulaOnUE/ZulaOnUE.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ZulaOnUE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ZulaOnUE, "ZulaOnUE" );
+
+DEFINE_LOG_CATEGORY(LogZulaOnUE)
